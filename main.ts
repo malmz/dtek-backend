18e1048b97f934ff1aa1c7b9a967d2fb3f64adbd
Carl Malmz <callem217@gmail.com>
2022-03-08 10:53:53 +0100

    add error handling, no more crashing :/

--- main.ts
+++ main.ts
@@ -1,10 +1,20 @@
-import { Application, logger, oakCors } from './deps.ts';
+import { Application, logger, oakCors, Status } from './deps.ts';
 import { router } from './src/router.ts';
 
 const app = new Application();
 app.use(logger.logger);
 app.use(logger.responseTime);
 app.use(oakCors());
+
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (error) {
+    ctx.response.status = Status.InternalServerError;
+    ctx.response.body = error.message;
+  }
+});
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 
